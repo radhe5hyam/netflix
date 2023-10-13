550d21d7627469dec2c3da629f158b48e209e2c0
Radheshyam Tard <rst.choudhary10@gmail.com>
2023-10-13 23:20:26 +0530

    prisma added and package updated

--- apps/admin/src/pages/api/hello.ts
+++ apps/admin/src/pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { PrismaClient } from '@fly-high/db'
+ 
+const client = new PrismaClient(); 
 type Data = {
   name: string
 }
